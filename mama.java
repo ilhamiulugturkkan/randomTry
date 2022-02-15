f0c9debe987d14d67ad1fc32033f4c5307140559
İlhami Uluğtürkkan <i.ulugturkkan@ug.bilkent.edu.tr>
2022-02-15 19:29:34 +0300

    Some changes

--- mama.java
+++ mama.java
@@ -2,6 +2,7 @@ public class mama {
 
     public static void main(String[] args){
         System.out.println("BROOOOOOOOO I'M LEARNIN' GIT!!!!");
+        System.out.println("Some changesssss....");
 
     }
 }
